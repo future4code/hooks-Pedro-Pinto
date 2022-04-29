3618c443e83172486131845a0fd8881ceb3188a0
pedrocesarp <pedrocesar5@gmail.com>
2022-04-28 22:42:27 -0300

    entrega-exercicio-28-04-2022

--- modulo3/insta4/src/App.js
+++ modulo3/insta4/src/App.js
@@ -10,30 +10,91 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-         <Post
-          nomeUsuario={'joãozinho'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
+  
 
-         <Post
-          nomeUsuario={'mairinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+state = { 
+  
+  posts: [
+
+{
+nomeUsuario: "Paulinha",
+fotoUsuario:  "XXX",
+fotoPost: "XXX"
+},
+
+{
+  nomeUsuario: "joãozinho",
+  fotoUsuario:  "XXX",
+  fotoPost: "XXX"
 
 
-      </MainContainer>
-    );
-  }
+},
+
+{
+  nomeUsuario: "mairinha",
+  fotoUsuario:  "XXX",
+  fotoPost: "XXX"
+
+},
+
+]        
+
+};
+
+render() {
+
+const listaPosts = this.state.posts.map((postes)=> {
+
+  return (
+<p>
+  {postes.nomeUsuario}
+  {postes.fotoUsuario}
+  {postes.fotoPost}
+</p>
+);
+
+});
+
+
+
+return (
+  <div>
+    <div> {listaPosts}  </div> 
+  </div>
+);
+}
 }
 
 export default App;
+
+
+
+// render() {
+
+
+//     return (
+//       <MainContainer>
+//         <Post
+//           nomeUsuario={'paulinha'}
+//           fotoUsuario={'https://picsum.photos/50/50'}
+//           fotoPost={'https://picsum.photos/200/150'}
+//         />
+//          <Post
+//           nomeUsuario={'joãozinho'}
+//           fotoUsuario={'https://picsum.photos/50/50'}
+//           fotoPost={'https://picsum.photos/200/151'}
+//         />
+
+//          <Post
+//           nomeUsuario={'mairinha'}
+//           fotoUsuario={'https://picsum.photos/50/50'}
+//           fotoPost={'https://picsum.photos/200/152'}
+//         />
+
+
+//       </MainContainer>
+//     );
+//   }
+// }
+
+// export default App;
