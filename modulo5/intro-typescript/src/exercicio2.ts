680eab9bbc0705028079890ad6cb68545414ed2a
pedrocesarp <pedrocesar5@gmail.com>
2022-08-09 22:19:15 -0300

    entrega-dia-09-08-22

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,10 @@
+
+function imprimeTresCoresFavoritas(cor1:string, cor2:string, cor3:string): string[] {
+
+    const arrayCores = []  
+    arrayCores.push(cor1,cor2,cor3)
+    return arrayCores
+   
+}
+
+console.log(imprimeTresCoresFavoritas("azul","verde","rosa"))
